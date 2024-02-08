1f017dcf4290ba61c77b7049b85a5a1c7c021fa3
Sam Machin <sam@sammachin.com>
2024-02-08 20:53:49 +0000

    optional object

--- src/containers/internal/views/phone-numbers/index.tsx
+++ src/containers/internal/views/phone-numbers/index.tsx
@@ -52,7 +52,7 @@ export const PhoneNumbers = () => {
   const [applyMassEdit, setApplyMassEdit] = useState(false);
   const [filter, setFilter] = useState("");
   const [accountSid, setAccountSid] = useState("");
-  const ADMIN_PHONENUMBER = import.meta.env.VITE_ADMIN_PHONENUMBER;
+  const ADMIN_PHONENUMBER = import.meta.env.VITE_ADMIN_PHONENUMBER?;
 
   const phoneNumbersFiltered = useMemo(() => {
     setAccountSid(getAccountFilter());
